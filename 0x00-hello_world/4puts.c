bebd71d83af9b2854cd80a0f1b054492e4a34cd0
CHIDERA <okaforchidera2000@gmail.com>
2022-04-07 12:50:24 -0700

    final commit

---
+++ 0x00-hello_world/4puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always (Success)
+ */
+ 
+ int main(void)
+ {
+ 	char str1[52];
+
+	strcpy(str1, "\"Programming is like building a multilingual puzzle");
+	puts(str1);
+
+	return (0);
+
+}
