20ab895b893cfbc64702cb3b14dcacd4642f7ca9
Anselm-Chidera <okaforchidera2000@gmail.com>
2022-04-08 12:09:08 -0700

    first commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,12 +16,12 @@
 int main(void)
 {
 	int n, l;
-	
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
 	l = n % 10;
-	
+
 	if (l < 6 && l != 0)
 	{
 	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
@@ -35,4 +35,3 @@ int main(void)
 	printf("Last digit of %d is %d and is 0\n", n, l);
 	}
 	return (0);
-}
