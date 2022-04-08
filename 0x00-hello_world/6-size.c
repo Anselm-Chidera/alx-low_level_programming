e46fdf55333d287ad88331c612b84786b0c1ed58
Anselm-Chidera <okaforchidera2000@gmail.com>
2022-04-08 03:48:20 -0700

    still clearing white spaces

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -20,8 +20,8 @@ int main(void)
 		printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 		printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
 		printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(e));
- 		printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(f));
+		printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(f));
 		printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(g));
-								
+
 		return (0);
 }
