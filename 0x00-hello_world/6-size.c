a5cef728cf455e875dd8c266ed4859cba892948b
Anselm-Chidera <okaforchidera2000@gmail.com>
2022-04-08 03:31:06 -0700

    i corrected liitle spaces

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,7 +8,7 @@
  *
  *    * Return: Always 0 (success)
  *
- *     */
+ *   */
 
 int main(void)
 
@@ -20,11 +20,11 @@ int main(void)
 		long long int f;
 		float g;
 
-
 		printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 		printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
-
 		printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(e));
- 		printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(f));										        printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(g));
-												return (0);
-										}
+ 		printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(f));
+		printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(g));
+								
+		return (0);
+}
