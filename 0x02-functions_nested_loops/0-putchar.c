6147847d45074bc5b3ae9b67a58bbc5c794be216
Anselm-Chidera <okaforchidera2000@gmail.com>
2022-04-12 10:18:03 -0700

    another commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#include <main.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (success)
  */
-int main(void)
+char main(void)
 {
-	putchar("_putchar\n")
+	putchar("_putchar\n");
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+char main(void)
+{
+	char x
+	for (x = a, x <= z, x++)
+
+		putchar("x\n");
+
+	return (0);
+}
