84c1fa393332933563e810f3546121e5c5a615aa
Anselm-Chidera <okaforchidera2000@gmail.com>
2022-05-11 03:36:48 -0700

    updates

---
+++ 0x0C-more_malloc_free/1-main.c
@@ -0,0 +1,20 @@
+es (16 sloc)  270 Bytes
+   
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *concat;
+
+    concat = string_nconcat("Best ", "School !!!", 6);
+    printf("%s\n", concat);
+    free(concat);
+    return (0);
+}
