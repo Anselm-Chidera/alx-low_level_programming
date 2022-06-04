54a5e1a871b1c2ff00a5c27e09ccea4da5a16e85
Anselm-Chidera <okaforchidera2000@gmail.com>
2022-06-04 07:08:57 -0700

    all linked

--- 0x12-singly_linked_lists/test/3-main.c
+++ 0x12-singly_linked_lists/test/3-main.c
@@ -39,7 +39,6 @@ int main(void)
 	add_node_end(&head, "Stuart");
 	add_node_end(&head, "Swati");
 	add_node_end(&head, "Timothy");
-	add_node_end(&head, "Victor");
 	add_node_end(&head, "Walton");
 	print_list(head);
 	return (0);
